26f7c11388431c02e164df2b27be3c3cea972537
ratul <sharker.ratul.08@gmail.com>
2015-04-10 12:53:41 +0600

    Example 6 was not compiling for the windows, that issue is fixed

--- compile_examples_windows.bat
+++ compile_examples_windows.bat
@@ -8,4 +8,4 @@ g++ Example_4_wait_notify.cpp Thread.cpp -lpthread -o Example_4_wait_notify.exe
 
 g++ Example_5_controlled_synchronizer.cpp Thread.cpp -lpthread -o Example_5_controlled_synchronizer.exe
 
-g++ Example_6_get_status Thread.cpp -lpthread -o Example_5_controlled_synchronizer.exe
+g++ Example_6_get_status Thread.cpp -lpthread -o Example_6_get_status Thread.exe
